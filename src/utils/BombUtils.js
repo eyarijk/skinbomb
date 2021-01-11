3fee746d2fa67f48fda05ea26b57af4286e1fc15
Yaroslav Ilnitskiy <eyarij@gmail.com>
2021-01-11 18:32:06 +0200

    bomb

--- src/utils/BombUtils.js
+++ src/utils/BombUtils.js
@@ -37,3 +37,33 @@ export function calcWingsStep(rate) {
 export function calcWingsFire(rate) {
   return rate >= 5.01;
 }
+
+export function stepRate(currentRate, rate) {
+  if (currentRate > 5) {
+    return rate / 30;
+  }
+
+  if (currentRate > 2) {
+    return rate / 40;
+  }
+
+  if (currentRate > 1.5) {
+    return 0.1;
+  }
+
+  return 0.08;
+}
+
+export function calcTime(rate) {
+  let time = 0;
+
+  for (
+    let currentRate = 0;
+    currentRate < rate;
+    currentRate += stepRate(currentRate, rate)
+  ) {
+    time += 50;
+  }
+
+  return time;
+}
